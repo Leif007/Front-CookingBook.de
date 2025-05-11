aa9c84824aee0ec4873e7fe1ac632fe9d4a848bd
Leif <leifstoehr333@gmail.com>
2025-05-11 15:02:09 +0200

    add HomeView und RecipeList

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  rezepte: {
+    id: number,
+    name: string,
+    ingredients: string[],
+    instructions: string
+  }[]
+}>()
+</script>
+
+<template>
+  <ul>
+    <li v-for="rezept in rezepte" :key="rezept.id">
+      <h3>{{ rezept.name }}</h3>
+      <p><strong>Zutaten:</strong> {{ rezept.ingredients.join(', ') }}</p>
+      <p><strong>Zubereitung:</strong> {{ rezept.instructions }}</p>
+    </li>
+  </ul>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import RecipeList from '../components/RecipeList.vue'
+
+const rezepte = [
+  {
+    id: 1,
+    name: 'Spaghetti Bolognese',
+    ingredients: ['Spaghetti', 'Hackfleisch', 'Tomatensauce'],
+    instructions: 'Alles anbraten, köcheln lassen und servieren.'
+  },
+  {
+    id: 2,
+    name: 'Pfannkuchen',
+    ingredients: ['Mehl', 'Milch', 'Eier'],
+    instructions: 'Zutaten vermischen und in der Pfanne ausbacken.'
+  }
+]
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Meine Rezepte</h1>
+    <RouterLink to="/add">➕ Neues Rezept erstellen</RouterLink>
+    <RecipeList :rezepte="rezepte" />
   </main>
 </template>
